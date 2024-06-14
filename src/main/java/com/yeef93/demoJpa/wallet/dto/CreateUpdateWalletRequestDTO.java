8e8f722073fa1bee5a9ee8f0da819cd82cb60d57
yeef93 <yunindafaranika@gmail.com>
2024-06-14 23:44:11 +0700

    feat:addvalidationwallet

--- src/main/java/com/yeef93/demoJpa/wallet/dto/CreateUpdateWalletRequestDTO.java
+++ src/main/java/com/yeef93/demoJpa/wallet/dto/CreateUpdateWalletRequestDTO.java
@@ -2,14 +2,20 @@ package com.yeef93.demoJpa.wallet.dto;
 
 import com.yeef93.demoJpa.users.entity.Users;
 import com.yeef93.demoJpa.wallet.entity.Wallet;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
 public class CreateUpdateWalletRequestDTO {
+    @NotBlank(message = "Name is required")
     private String name;
+    @NotNull(message = "Amount is required")
     private Double amount;
+    @NotNull(message = "Currency is required")
     private Integer currency;
-    private  long userId;
+    @NotNull(message = "User ID is required")
+    private  Long userId;
 
     public Wallet toEntity(){
         Wallet wallet = new Wallet();
